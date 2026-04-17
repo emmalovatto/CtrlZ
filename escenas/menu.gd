c78934b9e225925043995b33b9f7e62aaefd30c3
5i1 <5i1@cangallo.escuela>
2026-04-17 10:42:26 -0300

    cambios en el menu

--- escenas/menu.gd
+++ escenas/menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var boton_jugar = $CanvasLayer/VBoxContainer/boton_jugar
+@onready var boton_instrucciones = $CanvasLayer/VBoxContainer/boton_instrucciones
+@onready var boton_salir = $CanvasLayer/VBoxContainer/boton_salir
 
 func _ready() -> void:
 	$ConfirmarSalida.visible = false
 
 func _on_boton_jugar_button_up() -> void:
 	$menu_animacion.play("menu")
+	boton_jugar.visible = false
+	boton_instrucciones.visible = false
+	boton_salir.visible = false
 
 func _on_menu_animacion_animation_finished(anim_name):
 	if anim_name == "menu":
@@ -14,3 +20,6 @@ func _on_menu_animacion_animation_finished(anim_name):
 func _on_boton_salir_button_up() -> void:
 	$ConfirmarSalida.visible = true
 	get_tree().paused = true
+
+func _on_boton_opciones_button_up() -> void:
+	pass # Replace with function body.
