c7b97a8fd009ac18c3b9c6d7a24eb79711a7fd27
5i9 <5i9@cangallo.escuela>
2026-06-26 10:42:12 -0300

    musica.gd

---
+++ escenas/menu_ajustes/music.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+@export var musica: Texture2D
+@export var silencio: Texture2D
+
+var bus_index: int
+
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index("Music")
+	update_icon()
+
+func _pressed():
+	AudioServer.set_bus_mute(bus_index,!AudioServer.is_bus_mute(bus_index))
+	update_icon()
+
+func update_icon():
+	if AudioServer.is_bus_mute(bus_index):
+		texture_normal = silencio
+	else:
+		texture_normal = musica
